1b9f2eb5c006e92cee8f1db2457b2bf1b9dfee90
onik-999 <onikdas.dev@gmail.com>
2026-08-20 00:29:43 +0600

    feat(home): add animated food categories section to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Banner from "@/components/banner/Banner";
-import Image from "next/image";
+import FoodCategories from "@/components/categories/FoodCategories";
 
 export default function Home() {
   return (
     <div>
-      <Banner></Banner>
+      <Banner />
+      <FoodCategories />
     </div>
   );
 }
