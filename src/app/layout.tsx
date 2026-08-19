146b968bd097820773cd77152cf7c556453b12a2
souravnath4587-cmd <souravnath4587@gmail.com>
2026-08-20 00:08:40 +0600

    set up env variable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
+import dns from "node:dns";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/common/Navbar"; 
+import Navbar from "@/components/common/Navbar";
 import Footer from "@/components/common/Footer";
 
 const geistSans = Geist({
@@ -16,7 +19,8 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Food Flow — Online Food Delivery System",
-  description: "Order your favorite food from the best restaurants near you with Food Flow.",
+  description:
+    "Order your favorite food from the best restaurants near you with Food Flow.",
 };
 
 export default function RootLayout({
@@ -24,8 +28,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
-  const user = null; 
+  const user = null;
 
   return (
     <html
@@ -33,11 +36,8 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-gray-50 dark:bg-gray-950 text-gray-900 dark:text-gray-100">
-        
         <Navbar user={user} cartItemCount={0} />
-        <main className="flex-grow">
-          {children}
-        </main>
+        <main className="flex-grow">{children}</main>
         <Footer></Footer>
       </body>
     </html>
